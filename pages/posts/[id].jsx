6d187e9ae6514b88a4979c5f5b33db76e53fadea
Davis <kuntaytc@gmail.com>
2021-11-02 17:59:53 +0300

    css modules imports

--- pages/posts/[id].jsx
+++ pages/posts/[id].jsx
@@ -1,5 +1,8 @@
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import Head from 'next/head'
+import Date from '../../components/date' 
+import utilStyles from '../../styles/utils.module.css'
 
 export async function getStaticPaths() {
 	const paths = getAllPostIds();
@@ -21,13 +24,24 @@ export async function getStaticProps({ params }) {
 const Post = ({ postData }) => {
 	return (
 		<Layout>
-			{postData.title}
-			<br />
-			{postData.id}
-			<br />
-			{postData.date}
-			<br />
-			<div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+			<Head>
+				<title>{postData.title}</title>
+			</Head>
+
+			<article>
+				<h1 className={utilStyles.headingXl}>
+					{postData.title}
+				</h1>
+
+				<br />
+
+				<div className={utilStyles.lightText}>
+					<Date dateString={postData.date} />
+				</div>
+				
+				<br />
+				<div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+			</article>
 		</Layout>
 	);
 };
